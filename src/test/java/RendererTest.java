920798451aae49cf502fd3941ff7446990d2d5d0
Tobias Huber <huabatobs@gmail.com>
2017-04-04 11:45:23 +0200

    fixed tests

--- src/test/java/RendererTest.java
+++ src/test/java/RendererTest.java
@@ -26,8 +26,8 @@ public class RendererTest {
     @Test
     public void testRendering() throws Exception {
         assertEquals("Instance of edu.hm.huberneumeier.SomeClass:\n" +
-                "foo (Type int): 5\n" +
                 "array (Type int[]): [1, 2, 3]\n" +
+                "foo (Type int): 5\n" +
                 "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n", renderer.render());
     }
 
